e6abffe436f789f6be1a39cdd21966c634350e0e
Nuzumi <michal.sewerniak@o2.pl>
2018-06-16 20:50:30 +0200

    .

--- AssetStore/Assets/Scripts/ScriptableObjects/Items/Item.cs
+++ AssetStore/Assets/Scripts/ScriptableObjects/Items/Item.cs
@@ -14,13 +14,27 @@ public class Item : TileObject
 
     public void OnEquip(Character character)
     {
-        
+        for (int i = 0; i < modifierListPart1.Count; i++)
+        {
+            if (character.skillsPart1.Contains(modifierListPart1[i]))
+            {
+                character.skillsPart2[character.skillsPart1.IndexOf(modifierListPart1[i])] += modifierListPart2[i];
+            }
+            else
+            {
+                character.skillsPart1.Add(modifierListPart1[i]);
+                character.skillsPart2.Add(modifierListPart2[i]);
+            }
+        }
     }
 
 
     public void OnDequip(Character character)
     {
-        
+        for (int i = 0; i < modifierListPart1.Count; i++)
+        {
+            character.skillsPart2[character.skillsPart1.IndexOf(modifierListPart1[i])] -= modifierListPart2[i];
+        }
     }
 
 }
