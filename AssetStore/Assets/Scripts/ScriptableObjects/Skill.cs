22474b8b0763dbfc5284582b0b6cf0923f73c7ce
Milena <mil.lukasik@gmail.com>
2018-06-16 18:43:56 +0200

    Add Event and fix literówki :)

---
+++ AssetStore/Assets/Scripts/ScriptableObjects/Skill.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gameplay/Skill")]
+public class Skill : ScriptableObject {
+
+    public string SkillName;
+    public int Level;
+   
+}
