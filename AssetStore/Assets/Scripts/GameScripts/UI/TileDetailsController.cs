a2bc866a12766f51bb6f6199df22d934fff75177
Milena <mil.lukasik@gmail.com>
2018-06-16 22:36:26 +0200

    wtf

---
+++ AssetStore/Assets/Scripts/GameScripts/UI/TileDetailsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileDetailsController : MonoBehaviour {
+
+    TileEvent ShowDetails;
+
+    private string NamePrefix = "Name: ";
+    private string DescPrefix = "Description: ";
+
+    public Text NameText;
+    public Text DescText;
+
+    private void Awake()
+    {
+        ShowDetails.AddListener(ShowPanel);
+    }
+
+    private void OnDisable()
+    {
+        ShowDetails.RemoveListener(ShowPanel);
+    }
+
+    public void ShowPanel(TileObject tile)
+    {
+        //Włączenie/ wyłączenie panelu
+        gameObject.SetActive(!gameObject.activeSelf);
+        //Wypełnienie danych na texty
+        NameText.text = NamePrefix + tile.Name;
+        DescText.text = DescText + tile.Description;
+    }
+}
---
+++ AssetStore/Assets/Scripts/ScriptableObjects/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gameplay/Skill")]
+public class Skill : TileObject
+{
+    
+    public Sprite skillSprite;
+    
+    public Skill strongFor;
+
+    public Skill weekFor;
+
+}
