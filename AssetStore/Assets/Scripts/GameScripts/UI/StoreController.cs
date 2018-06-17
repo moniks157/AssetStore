28b0578779cda283de3270fff84dcde0ab9e4e37
Unknown <ania666666@op.pl>
2018-06-17 10:50:08 +0200

    strore1

--- AssetStore/Assets/Scripts/GameScripts/UI/StoreController.cs
+++ AssetStore/Assets/Scripts/GameScripts/UI/StoreController.cs
@@ -1,55 +1,126 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class StoreController : MonoBehaviour {
     public GameObject storeObject;
     static int PRICE_INDEX=5;
-    public const float offsetY = 1.9f;
+
+    public GameObject hero1;
+    public GameObject hero2;
+    public GameObject hero3;
+
+    public GameObject item1;
+    public GameObject item2;
+    public GameObject item3;
+
+
     // Use this for initialization
+
     void Awake () {
+
+
+        System.Random r = new System.Random();
         DataContainer dataContainer = GameObject.FindObjectOfType<DataContainer>();
+        
         if (dataContainer == null)
         {
             Debug.Log("null list");
         }
         else
         {
-            Debug.Log("inicjacja"+ dataContainer.notBoughtCharacters.Count);
-            for (int i = 0; i < dataContainer.notBoughtCharacters.Count; i++)
-            {
-                Debug.Log("inicjacja");
-                GameObject newStoredObject = Instantiate(storeObject);
-                gameObject.transform.GetChild(0).GetComponentInChildren<StoreTailController>().init(dataContainer.notBoughtCharacters[i]);
-                Debug.Log(dataContainer.notBoughtItems[i].Name);
-                gameObject.transform.GetComponentInChildren<TitleStore>().ObjectName.text = dataContainer.notBoughtCharacters[i].Name;
-                gameObject.transform.GetComponentInChildren<TitleStore>().Price.text = dataContainer.notBoughtCharacters[i].SkillValueSum * PRICE_INDEX + "@";
-
-                Vector3 startPos = storeObject.transform.position;
-
-                    float posY = (offsetY * i) + startPos.y;
-                    gameObject.transform.position = new Vector3(startPos.x, posY, startPos.z);
-            }
-            for (int i = 0; i < dataContainer.notBoughtItems.Count; i++)
+            List<int> indexsList = new List<int>();
+            if (dataContainer.notBoughtCharacters.Count > 2)
             {
 
-                Debug.Log("inicjacja");
-                GameObject newStoredObject = Instantiate(storeObject);
-                gameObject.transform.GetChild(0).GetComponentInChildren<StoreTailController>().init(dataContainer.notBoughtItems[i]);
-                Debug.Log(dataContainer.notBoughtItems[i].Name);
-                gameObject.transform.GetComponentInChildren<TitleStore>().ObjectName.text = dataContainer.notBoughtItems[i].Name;
-                gameObject.transform.GetComponentInChildren<TitleStore>().Price.text = dataContainer.notBoughtItems[i].ItemValueSum* PRICE_INDEX+"@";
-                Vector3 startPos = storeObject.transform.position;
+                while (indexsList.Count < 3)
+                {
+                    Debug.Log("while");
+                    int index = r.Next(0, dataContainer.notBoughtCharacters.Count);
+                    if (!indexsList.Contains(index))
+                    {
+                        indexsList.Add(index);
+                    }
+                }
+                Debug.Log("init method");
+                hero1.GetComponent<TitleStore>().init(dataContainer.notBoughtCharacters[indexsList[0]], false);
+                hero2.GetComponent<TitleStore>().init(dataContainer.notBoughtCharacters[indexsList[1]], false);
+                hero3.GetComponent<TitleStore>().init(dataContainer.notBoughtCharacters[indexsList[2]], false);
+            }else
+            {
+                if (dataContainer.notBoughtCharacters.Count == 1)
+                {
+                    hero1.GetComponent<TitleStore>().init(dataContainer.notBoughtCharacters[indexsList[0]], false);
+                    hero2.SetActive(false);
+                    hero3.SetActive(false);
+                } 
+                if (dataContainer.notBoughtCharacters.Count ==0)
+                {
+                    hero1.SetActive(false);
+                    hero2.SetActive(false);
+                    hero3.SetActive(false);
 
-                float posY = (offsetY * i) + startPos.y;
-                gameObject.transform.position = new Vector3(startPos.x, posY, startPos.z);
-
+                }
+                else
+                {
+                    hero1.GetComponent<TitleStore>().init(dataContainer.notBoughtCharacters[indexsList[0]], false);
+                    hero2.GetComponent<TitleStore>().init(dataContainer.notBoughtCharacters[indexsList[1]], false);
+                    hero3.SetActive(false);
+                }
             }
+
+            indexsList.Clear();
+
+            if (dataContainer.notBoughtItems.Count > 2)
+            {
+        
+                while (indexsList.Count < 3)
+                {
+                    Debug.Log("while");
+                    int index = r.Next(0, dataContainer.notBoughtItems.Count);
+                    if (!indexsList.Contains(index))
+                    {
+                        indexsList.Add(index);
+                    }
+                }
+                Debug.Log("init method");
+            item1.GetComponent<TitleStore>().init(dataContainer.notBoughtItems[indexsList[0]], true);
+            item2.GetComponent<TitleStore>().init(dataContainer.notBoughtItems[indexsList[1]], true);
+            item3.GetComponent<TitleStore>().init(dataContainer.notBoughtItems[indexsList[2]], true);
+        }
+            else
+            {
+                if (dataContainer.notBoughtItems.Count == 1)
+                {
+                    item1.GetComponent<TitleStore>().init(dataContainer.notBoughtItems[indexsList[0]], true);
+                    item2.SetActive(false);
+                    item3.SetActive(false);
+                }
+                if (dataContainer.notBoughtItems.Count == 0)
+                {
+                    item1.SetActive(false);
+                    item2.SetActive(false);
+                    item3.SetActive(false);
+
+                }
+                else
+                {
+                    item1.GetComponent<TitleStore>().init(dataContainer.notBoughtItems[indexsList[0]], true);
+                    item2.GetComponent<TitleStore>().init(dataContainer.notBoughtItems[indexsList[1]], true);
+                    item3.SetActive(false);
+                }
+            }
+
+            indexsList.Clear();
+            
+           
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   public void Buy()
+    {
+        
+    }
+
 }
--- AssetStore/Assets/Scripts/GameScripts/UI/TitleStore.cs
+++ AssetStore/Assets/Scripts/GameScripts/UI/TitleStore.cs
@@ -8,5 +8,26 @@ public class TitleStore : MonoBehaviour {
 	public Text ObjectName;
     public Text Price;
 
+    public int PRICE_INDEX = 5;
+   public void init(TileObject tile,bool isItem)
+    {
+        ObjectName.text = tile.Name ?? "";
+
+        gameObject.transform.GetComponentInChildren<StoreTailController>().init(tile);
+        if (isItem)
+        {
+            Price.text = (tile as Item).ItemValueSum* PRICE_INDEX+"@";
+
+        }
+        else
+        {
+           
+            Price.text =( tile as Character).SkillValueSum * PRICE_INDEX + "@";
+        }
+
+
+
+    }
+
 
 }
